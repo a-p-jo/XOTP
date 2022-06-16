1b7da2ffba49147cb86f6e542b07177671d7bec8
A.P. Jo <66941562+a-p-jo@users.noreply.github.com>
2022-06-17 03:37:45 +0530

    v2.6
    Removed srcfile()/dstfile() mess, just made those xotpState fields instead.
    Renamed *_filename members to *file_name.
    Formatting improvements
    Added the P_EMSG_X, P_SYSE_DO, P_SYSE_X macros for fatal error reporting and exiting.
    Added missing `;` after RtlGenRandom() in getpad()

--- xotp.c
+++ xotp.c
@@ -26,9 +26,9 @@ typedef struct {
 	is_writing_to_pad : 1, is_writing_to_default_pad : 1,
 	is_writing_to_outfile : 1;
 
-	char *in_filename, *pad_filename, *out_filename;
+	char *infile_name, *padfile_name, *outfile_name;
 
-	FILE *plainfile, *cipherfile, *padfile;
+	FILE *infile, *padfile, *outfile;
 	void *tofree;
 } xotpState;
 
@@ -44,115 +44,118 @@ static inline char *strdupcat(const char *s1, const char *s2)
 
 static inline void errexit(const xotpState *state)
 {
-	if (state->pad_filename && (state->is_writing_to_pad || state->is_writing_to_default_pad))
-		remove(state->pad_filename);
-	if (state->out_filename && state->is_writing_to_outfile)
-		remove(state->out_filename);
+	if (
+		state->padfile_name && (state->is_writing_to_pad 
+		|| state->is_writing_to_default_pad)
+	)
+		remove(state->padfile_name);
+	if (state->outfile_name && state->is_writing_to_outfile)
+		remove(state->outfile_name);
 	free(state->tofree);
 	exit(EXIT_FAILURE);
 }
 
-static inline bool streq(const char *s1, const char *s2) { return strcmp(s1, s2) == 0; }
+static inline bool streq(const char *s1, const char *s2)
+{
+	return strcmp(s1, s2) == 0;
+}
 
-static inline FILE **srcfile(const xotpState *state)
-{
-	return (FILE **) (state->is_encrypting ? &state->plainfile : &state->cipherfile); /* const cast */
-}
-static inline FILE **dstfile(const xotpState *state)
-{
-	return (FILE **) (state->is_encrypting ? &state->cipherfile : &state->plainfile); /* const cast */
-}
+/* Print error message and exit */
+#define P_EMSG_X(...) \
+	fprintf(stderr, "Error : "__VA_ARGS__), exit(EXIT_FAILURE)
+
+/* Print system error message and do action */
+#define P_SYSE_DO(action, ...)  do {                                   \
+	fprintf(stderr, "Error : "__VA_ARGS__);                        \
+	int e = errno;                                                 \
+	if (e) fprintf(stderr, "System Message : %s.\n", strerror(e)); \
+	action;                                                        \
+} while (0)
+
+/* Print system error message and exit */
+#define P_SYSE_X(...) P_SYSE_DO(exit(EXIT_FAILURE), __VA_ARGS__)
 
 /* Requires argc >= 2 */
 static xotpState xotpState_init(int argc, char **argv)
 {
-	xotpState state = {.is_generating_pad = true, .is_encrypting = streq("e", argv[1])};
+	xotpState state = {
+		.is_generating_pad = true, 
+		.is_encrypting = streq("e", argv[1])
+	};
 
 	if (!state.is_encrypting) {
 		if (streq("d", argv[1])) 
 			state.is_generating_pad = false;
-		else {
-			fprintf(stderr, "Error : Unrecognised argument : \"%s\".\n" 
-				"Only \"e\" and \"d\" are valid modes.\n", argv[1]);
-			exit(EXIT_FAILURE);
-		}
+		else
+			P_EMSG_X(
+				"Unrecognised argument : \"%s\".\n"
+				"Only \"e\" and \"d\" are valid modes.\n", argv[1]
+			);
 	}
 
 	/* Search argv for options. Once found, subsequent matches ignored. */
 	for (int i = 2; i < argc; i++) {
-		if (!state.in_filename && streq("-f", argv[i]))
-			state.in_filename = argv[++i];
-		else if (!state.pad_filename && streq("-p", argv[i]))
-			state.pad_filename = argv[++i];
-		else if (!state.out_filename && streq("-o", argv[i]))
-			state.out_filename = argv[++i];
+		if (!state.infile_name && streq("-f", argv[i]))
+			state.infile_name = argv[++i];
+		else if (!state.padfile_name && streq("-p", argv[i]))
+			state.padfile_name = argv[++i];
+		else if (!state.outfile_name && streq("-o", argv[i]))
+			state.outfile_name = argv[++i];
 
-		if (state.in_filename && state.pad_filename && state.out_filename)
+		if (state.infile_name && state.padfile_name && state.outfile_name)
 			break;				
 	}
 
-	if (state.in_filename) {
-		if (!( *srcfile(&state) = fopen(state.in_filename, "rb") )) {
-			fprintf(stderr,"Error : Could not open file \"%s\"%s%s\n",
-					state.in_filename, errno? " : " : ".",
-					errno? strerror(errno) : "");
-			exit(EXIT_FAILURE);
-		}
+	if (state.infile_name) {
+		if (!( state.infile = fopen(state.infile_name, "rb") ))
+			P_SYSE_X("Could not open file \"%s\".\n", state.infile_name);
 	} else 
-		*srcfile(&state) = stdin;
+		state.infile = stdin;
 
-	if (state.pad_filename) {
-		state.padfile = fopen(state.pad_filename, "rb");
+	if (state.padfile_name) {
+		state.padfile = fopen(state.padfile_name, "rb");
 		state.is_generating_pad = false;
 
 		if (state.is_encrypting && !state.padfile) {
-			state.padfile = fopen(state.pad_filename, "wb");
+			state.padfile = fopen(state.padfile_name, "wb");
 			state.is_writing_to_pad = true;
 			state.is_generating_pad = true;
 		}
-		if (!state.padfile) {
-			fprintf(stderr, "Error : Could not open pad \"%s\"%s%s\n",
-					state.pad_filename, errno? " : " : ".",
-					errno? strerror(errno) : "");
-			exit(EXIT_FAILURE);
-		}
+		if (!state.padfile)
+			P_SYSE_X("Could not open pad \"%s\".\n", state.padfile_name);
 	} else {
 		if (state.is_encrypting) {
 			state.is_writing_to_default_pad = true;
 
-			if (!( state.pad_filename = strdupcat(state.in_filename? state.in_filename : DEFAULT_PAD_FILENAME, DEFAULT_PAD_EXTENSION) )) {
-				fprintf(stderr, "Error : Could not malloc for pad filename.\n");
-				exit(EXIT_FAILURE);
-			}
+			if (!(
+				state.padfile_name = strdupcat(state.infile_name?
+					state.infile_name : DEFAULT_PAD_FILENAME, DEFAULT_PAD_EXTENSION)
+			))
+				P_SYSE_X("Could not malloc for pad filename.\n");
 
-			if (!( state.padfile = fopen(state.pad_filename, "wb") )) {
-				fprintf(stderr, "Error : Could not open \"%s\" to save pad%s%s\n",
-						state.pad_filename, errno? " : " : ".",
-						errno? strerror(errno) : "Unknown error");
-				free(state.pad_filename);
-				exit(EXIT_FAILURE);
+			if (!( state.padfile = fopen(state.padfile_name, "wb") )) {
+				P_SYSE_DO(
+					free(state.padfile_name); exit(EXIT_FAILURE),
+					"Could not open \"%s\" to save pad.\n", state.padfile_name
+				);
 			}
-			state.tofree = state.pad_filename;
-		} else if (state.in_filename)
+			state.tofree = state.padfile_name;
+		} else if (state.infile_name)
 			state.padfile = stdin;
-		else {
-			fprintf(stderr, "Error : No pad specified.\n");
-			exit(EXIT_FAILURE);
-		}
+		else
+			P_EMSG_X("No pad specified.\n");
 	}
 
-	if (state.out_filename) {
+	if (state.outfile_name) {
 		state.is_writing_to_outfile = true;
 
-		if (!( *dstfile(&state) = fopen(state.out_filename, "wb") )) {
-			fprintf(stderr,"Error : Could not open \"%s\" to write output%s%s\n",
-					state.out_filename, errno? " : " : ".",
-					errno? strerror(errno) : "");
-			state.out_filename = NULL; /* errexit() won't remove(oname) if NULL */
-			errexit(&state);
-		}
+		if (!( state.outfile = fopen(state.outfile_name, "wb") ))
+			P_SYSE_DO(
+				state.outfile_name = NULL; errexit(&state),
+				"Could not open \"%s\" to write output.\n", state.outfile_name
+			);
 	} else
-		*dstfile(&state) = stdout;
+		state.outfile = stdout;
 
 	return state;
 }
@@ -163,13 +166,13 @@ static inline bool getpad(xotpState *state, byte *restrict dst, FILE *src, size_
 {
 	if (state->is_generating_pad) {
 		#ifdef OS_WIN
-       			return RtlGenRandom(dst, nbytes)
+       		return RtlGenRandom(dst, nbytes);
 		#elif defined OS_NIX
-			return fread(dst, 1, nbytes, src) == nbytes;
+		return fread(dst, 1, nbytes, src) == nbytes;
 		#else
-			for (size_t i = 0; i < nbytes; i++)
-				dst[i] = rand() % (UCHAR_MAX+1);
-			return true;
+		for (size_t i = 0; i < nbytes; i++)
+			dst[i] = rand() % (UCHAR_MAX+1);
+		return true;
 		#endif
 	} else
 		return fread(dst, 1, nbytes, state->padfile) == nbytes;
@@ -178,75 +181,47 @@ static inline bool getpad(xotpState *state, byte *restrict dst, FILE *src, size_
 int main(int argc, char **argv)
 {
 	if (argc >= 2 && !streq("-h", argv[1]) && !streq("--help", argv[1])) {
+
 		xotpState state = xotpState_init(argc, argv);
 		byte buf1[IO_BLKSZ], buf2[IO_BLKSZ];
-		FILE *src = *srcfile(&state), *dst = *dstfile(&state), *randf = NULL;
+		FILE *randf = NULL;
 		
-		#ifdef OS_NIX
-			if (!( randf = fopen(NIX_RAND_DEV, "rb") )) {
-				fprintf(stderr, "Error : Could not open "NIX_RAND_DEV" to generate pad%s%s\n",
-						errno? " : " : ".",
-						errno? strerror(errno) : "");
-				errexit(&state);
-			}
+		#ifdef OS_NIX	
+		if (!( randf = fopen(NIX_RAND_DEV, "rb") ))
+			P_SYSE_DO(errexit(&state), "Could not open "NIX_RAND_DEV" to generate pad.\n");
 		#elif defined OS_OTHER
-			srand(time(0));
+		srand(time(0));
 		#endif
 
 		
-		while (!feof(src)) {
-			size_t nsrc = fread(buf1, 1, sizeof(buf1), src);
-			
-			if (ferror(src)) {
-				fprintf(stderr,"Error reading input%s%s\n",
-						errno? " : " : ".",
-						errno? strerror(errno) : "");
-				errexit(&state);
-			}
-			
-			if (!getpad(&state, buf2, randf, nsrc)) {
-				fprintf(stderr, "Error getting sufficient pad data%s%s\n",
-						errno? " : " : ".",
-						errno? strerror(errno) : "");
-				errexit(&state);
-			}
-			
-			size_t npad;
-			if (state.is_generating_pad && (npad = fwrite(buf2, 1, nsrc, state.padfile)) != nsrc) {		
-				fprintf(stderr,"Error saving pad data%s%s\n",
-						errno? " : " : ".",
-						errno? strerror(errno) : "");
-				errexit(&state);
-			}
+		while (!feof(state.infile)) {
+			size_t nread = fread(buf1, 1, sizeof(buf1), state.infile);
 
-			for (size_t i = 0; i < nsrc; i++)
+			if (ferror(state.infile))
+				P_SYSE_DO(errexit(&state), "Failed reading input.\n");
+			
+			if (!getpad(&state, buf2, randf, nread))
+				P_SYSE_DO(errexit(&state), "Failed getting pad data.\n");
+			
+			if (state.is_generating_pad && fwrite(buf2, 1, nread, state.padfile) != nread)		
+				P_SYSE_DO(errexit(&state), "Failed writing pad data.\n");
+
+			for (size_t i = 0; i < nread; i++)
 				buf1[i] ^= buf2[i];
 
-			size_t ndst = fwrite(buf1, 1, nsrc, dst);
-			if (ndst != nsrc) {
-				fprintf(stderr, "Error writing output%s%s\n",
-						errno? " : " : ".",
-						errno? strerror(errno) : "");
-				errexit(&state);
-			}
+			if (fwrite(buf1, 1, nread, state.outfile) != nread)
+				P_SYSE_DO(errexit(&state), "Failed writing output.\n"); 
 		}
 
 		free(state.tofree);
-		if (fclose(dst) == EOF) {
-			fprintf(stderr,"Error saving output%s%s\n",
-					errno? " : " : ".",
-					errno? strerror(errno) : "");
-			exit(EXIT_FAILURE);
-		}
-		if (state.is_generating_pad && fclose(state.padfile) == EOF) {
-			fprintf(stderr,"Error saving pad%s%s\n",
-					errno? " : " : ".",
-					errno? strerror(errno) : "");
-			exit(EXIT_FAILURE);
-		} else
+		if (fclose(state.outfile) == EOF)
+			P_SYSE_X("Failed saving output.\n");
+		if (state.is_generating_pad && fclose(state.padfile) == EOF)
+			P_SYSE_X("Failed saving pad.\n");
+		else
 			exit(EXIT_SUCCESS);
-	} else {
-		fprintf(stderr,
+	} else
+		P_EMSG_X(
 		"Invalid arguments, see usage below for help.\n\n"
 		"Simple Usage :\n"
 		"-> \"xotp e -f foo -o foo.xotp\"\n"
@@ -272,7 +247,6 @@ int main(int argc, char **argv)
 		"\t3. \"-o\" : Give output file.\n"
 		"\tWithout \"-o\", output is written to stdout.\n"
 		"\n"
-		"See https://github.com/a-p-jo/XOTP for more information.\n");
-		exit(EXIT_SUCCESS);
-	}
+		"See https://github.com/a-p-jo/XOTP for more information.\n"
+		);
 }
